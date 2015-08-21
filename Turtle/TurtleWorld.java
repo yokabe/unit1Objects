bb0fa1ef184c632ae566a26a44807a1d854ffed9
Yudai Okabe <yokabe@NN123-HA-14.HS.sd203.org>
2015-08-21 15:07:58 -0500

    More practice with Turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,19 +6,59 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
+        
+        
+        int degree = 0;
+        int forward = 0;
+        int connor = 1;
+        int connoR = 1;
+        int Connor = 1;
+        int cOnnor = 1;
+        int COnnor = 1;
         turtle.penDown();
-        
-        while (degree <= 180)
+        while (connor <=1000)
         {
+            turtle.forward(10);
+            turtle.turn(10);
+            connor = connor + 1;
             
-       
-       
-        
-        turtle.forward(50);
-        turtle.turn(45);
-        turtle.forward(100);
-        
-       
-    }
+        };
+        turtle.turn(90);
+        turtle.setPenColor(Color.RED);
+        while (connoR <=1000)
+        {
+            turtle.forward(10);
+            turtle.turn(10);
+            connoR = connoR + 1;
+            
+        };
+        turtle.turn(90);
+        turtle.setPenColor(Color.PINK);
+        while (Connor <=1000)
+        {
+            turtle.forward(10);
+            turtle.turn(10);
+            Connor = Connor + 1;
+            
+        };
+        turtle.turn(90);
+        turtle.setPenColor(Color.GREEN);
+        while (cOnnor <=1000)
+        {
+            turtle.forward(10);
+            turtle.turn(10);
+            cOnnor = cOnnor + 1;
+            
+        };
+        turtle.turn(90);
+        turtle.setPenColor(Color.MAGENTA);
+        while (COnnor <=1000)
+        {
+            turtle.forward(10);
+            turtle.turn(10);
+            COnnor = COnnor + 1;
+            
+        };
+   }
 }
